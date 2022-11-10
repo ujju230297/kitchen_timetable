be23970979059462c7305a527b887a596752a49d
ujju230297 <59918782+ujju230297@users.noreply.github.com>
2022-11-10 11:08:17 -0800

    Add files via upload

---
+++ kitchen_timetable.java
@@ -0,0 +1,38 @@
+/* package codechef; // don't place package name! */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+public class Main
+{
+    public static void main (String[] args)
+    {
+        // your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[] = new int[n];
+		int count=0;
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		
+		int b[] = new int[n];
+		for(int i=1;i<n;i++)
+		{
+			b[i]=sc.nextInt();
+		}
+		
+		for(int i=0;i<n;i++)
+		{
+			if(b[i]<(a[i+1]-a[i]))
+			{
+				count++;
+			}
+		}
+		System.out.println(count);
+		
+		
+    }
+}
