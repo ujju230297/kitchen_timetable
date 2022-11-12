d54c1bfacf237b2db3a9ddd43d032de7ef50331d
ujju230297 <59918782+ujju230297@users.noreply.github.com>
2022-11-11 21:51:48 -0800

    Update kitchen_timetable.java

--- kitchen_timetable.java
+++ kitchen_timetable.java
@@ -13,8 +13,8 @@ public class Main
 		int n=sc.nextInt();
 		int a[] = new int[n];
 		int count=0;
-		a[0]=0;
-		for(int i=1;i<n;i++)
+	
+		for(int i=0;i<n;i++)
 		{
 			a[i]=sc.nextInt();
 		}
@@ -24,14 +24,14 @@ public class Main
 		{
 			b[i]=sc.nextInt();
 		}
-		if(b[0]<a[0])
+		if(b[0]<=a[0])
 		{
 			count++;
 		}
 		
 		for(int i=1;i<n;i++)
 		{
-			if(b[i]<(a[i+1]-a[i]))
+			if(b[i]<=(a[i]-a[i-1]))
 			{
 				count++;
 			}
